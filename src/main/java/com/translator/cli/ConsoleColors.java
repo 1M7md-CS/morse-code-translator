e1f97e525a6a80df51e4666eb6ad0817b13ec529
1M7md-CS <mohammadhammad464@gmail.com>
2025-05-21 23:32:49 +0300

    Removed unused colors in the ConsoleColors class

--- src/main/java/com/translator/cli/ConsoleColors.java
+++ src/main/java/com/translator/cli/ConsoleColors.java
@@ -6,7 +6,6 @@ public class ConsoleColors {
 	public static final String RESET = "\u001B[0m";
 	public static final String BOLD = "\u001B[1m";
 
-	public static final String WHITE = "\u001B[37m";
 	public static final String LIGHT_BLUE = "\u001B[96m";
 	public static final String LIGHT_PURPLE = "\u001B[95m";
 	public static final String LIGHT_GREEN = "\u001B[92m";
@@ -14,7 +13,7 @@ public class ConsoleColors {
 	public static final String LIGHT_GRAY = "\u001B[37m";
 	public static final String BRIGHT_YELLOW = "\u001B[93m";
 	public static final String BRIGHT_RED = "\u001B[91m";
-	public static final String DARK_BLUE = "\u001B[94m";
+
 
 	public static final String FRAME_COLOR = LIGHT_GRAY;
 	public static final String TITLE_COLOR = LIGHT_BLUE + BOLD;
